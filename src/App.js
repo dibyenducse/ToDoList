a47ebdb008c8bc497015cf9977c3406b9d23f680
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-03-06 09:37:21 +0600

    df

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function ToDoList(){
+  [tasks, setTasks] = useState(initialTasks);
+  
+  function handleAddTasks(text){
+    return(setTasks([
+      {
+        ...tasks,
+      id:taskId++,
+      text:{text},
+      done: true
+      }]
+      )
+   )
+  }
+
+
+  function handleChangeTasks(){
+
+    return(
+      setTasks()
+    )
+
+  }
+
+  function handleDeleteTasks(){
+    return(
+      setTasks()
+    )
+  }
+
 }
 
-export default App;
+
+
+let taskId = 3;
+const initialTasks = [
+  {id:0, text:"hello world", done: false},
+  {id:1, text:"hello moon", done: false},
+  {id:2, text:"hello sun", done: false},
+]
